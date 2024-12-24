2fe9f14c786e490baa6402b6d27bb8154337df08
Muhammad Abu Hussain Rifad <hussain.rifad89@gmail.com>
2024-12-25 00:38:56 +0300

    main

---
+++ triangle.py
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+def area(a, h):
+    return a*h/2
+
+def perimeter(a, b, c):
+    return (a+b+c)
+
+=======
+def area(a, b, c):
+    return (a + b + c) / 2
+
+
+def perimeter(a, b, c):
+    return a + b + c
+>>>>>>> b5b0fae727ca72c317c383b39c0af73d6adcd81c
